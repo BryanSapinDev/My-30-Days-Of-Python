d9bd5fa37dcb552784a5507400d8b6f2e8993a2e
BryanSapin <bryan.sapin@iut-rodez.fr>
2026-07-07 16:45:38 +0200

    End of day 1

---
+++ day_1/helloworld.py
@@ -0,0 +1,43 @@
+# 1.1/
+# python --version
+
+# 1.2/
+print(10 + 4)
+print(10 - 4)
+print(10 * 10)
+print(10 / 4)
+print(10 % 4)
+print(10**2)
+print(10//4)
+
+# 1.3/
+print('Bryan')
+print('Sapin')
+print('France')
+print("I am enjoying 30 days of python")
+
+# 1.4/
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Bryan', 'Java', 'France']))
+print(type('Bryan'))
+
+# 3.1 Write an example for different Python data types 
+# such as Number(Integer, Float, Complex), String, Boolean, List, Tuple, Set and Dictionary.
+5
+3.14
+4j
+"Bonjour"
+True
+["Bryan", "Sapin"]
+("Bryan", "Sapin")
+{"Bryan", "Sapin"}
+{
+    "prenom" : "Bryan",
+    "nom" : "Sapin"
+}
+
+# 3.2 Find an Euclidean distance between (2, 3) and (10, 8)
+print("The distance beetween (2, 3) and (10, 8) is (" + str(10-8) + "," + str(8-3) + ")")
